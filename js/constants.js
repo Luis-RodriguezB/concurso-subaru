6fc282d2014fada85d6edf58aec94c3cdc05f74c
LuisER5 <luis.e.rodriguez@accenture.com>
2023-10-19 16:43:04 -0600

    Refactoring, css theme

--- js/constants.js
+++ js/constants.js
@@ -1,8 +1,12 @@
-export default {
+const IS_DARK_SCHEMEMATCH = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const constants = {
   theme: {
     THEME_KEY: 'theme',
     DARK_THEME: 'dark-theme',
     LIGHT_THEME: 'light-theme',
   },
-  CHANGE_ICON_TIME: 500, 
+  CHANGE_ICON_TIME: 300,
+  IS_DARK_SCHEMEMATCH, 
 };
+export default constants;
--- js/main.js
+++ js/main.js
@@ -7,6 +7,13 @@ import {
   INPUTS_FORM_GROUP,
 } from './HTMLElements';
 
+const { 
+  theme: { DARK_THEME, LIGHT_THEME, THEME_KEY },
+  IS_DARK_SCHEMEMATCH,
+  CHANGE_ICON_TIME
+} = constants;
+
+
 document.onreadystatechange = function () {
   const isLoaded = document.readyState === 'complete';
 
@@ -21,57 +28,42 @@ window.onload = function () {
 };
 
 function onLoadTheme() {
-  const theme = localStorage.getItem('theme') || '';
-  switch (theme) {
-    case constants.theme.DARK_THEME:
-      BODY_ELEMENT.dataset.theme = constants.theme.DARK_THEME;
-      SUN_ICON.classList.add('show', 'wrapper__icon-showing');
-      CHANGE_THEME_BTN.checked = true;
-      break;
+  const theme = localStorage.getItem(THEME_KEY) || '';
+  const isDarkMode = (theme.length === 0 && IS_DARK_SCHEMEMATCH) || (theme === DARK_THEME);
 
-    default:
-      BODY_ELEMENT.dataset.theme = constants.theme.LIGHT_THEME;
-      MOON_ICON.classList.add('show', 'wrapper__icon-showing');
-      break;
+  if (isDarkMode) {
+    SUN_ICON.classList.add('show', 'wrapper__icon-showing');
+  } else {
+    MOON_ICON.classList.add('show', 'wrapper__icon-showing');
+    BODY_ELEMENT.classList.toggle(LIGHT_THEME);
   }
 }
 
 CHANGE_THEME_BTN.addEventListener('click', (e) => {
-  const isChecked = e.target.checked;
-  e.target.disabled = true;
+  const currentTheme = localStorage.getItem(THEME_KEY);
+  const nextTheme = currentTheme === LIGHT_THEME ? DARK_THEME : LIGHT_THEME;
 
-  localStorage.setItem(
-    'theme',
-    isChecked ? constants.theme.DARK_THEME : constants.theme.LIGHT_THEME
-  );
-  BODY_ELEMENT.dataset.theme = isChecked
-    ? constants.theme.DARK_THEME
-    : constants.theme.LIGHT_THEME;
+  localStorage.setItem(THEME_KEY, nextTheme);
 
-  if (isChecked) {
+  if (IS_DARK_SCHEMEMATCH) {
+    BODY_ELEMENT.classList.toggle(LIGHT_THEME);
+  } else {
+    BODY_ELEMENT.classList.toggle(DARK_THEME);
+  }
+
+  if (nextTheme === DARK_THEME) {
     MOON_ICON.classList.add('wrapper__icon-hidding');
 
     setTimeout(() => {
       MOON_ICON.classList.remove('show', 'wrapper__icon-hidding');
       SUN_ICON.classList.add('show');
-      e.target.disabled = false;
-    }, constants.CHANGE_ICON_TIME);
+    }, CHANGE_ICON_TIME);
   } else {
     SUN_ICON.classList.add('wrapper__icon-hidding');
 
     setTimeout(() => {
       SUN_ICON.classList.remove('show', 'wrapper__icon-hidding');
       MOON_ICON.classList.add('show');
-      e.target.disabled = false;
-    }, constants.CHANGE_ICON_TIME);
+    }, CHANGE_ICON_TIME);
   }
-});
-
-INPUTS_FORM_GROUP.forEach((INPUT) => {
-  INPUT.addEventListener('focusout', (event) => {
-    const value = event.target.value;
-    const isEmpty = value.trim().length === 0;
-
-    INPUT.querySelector('.form-label').style.bottom = isEmpty ? '6px' : '30px';
-  });
 });
