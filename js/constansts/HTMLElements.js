c3836c24b0341f3b7dab5d11aef53fee7c46ac90
LuisER5 <luis.e.rodriguez@accenture.com>
2023-11-01 16:42:45 -0600

    ...

--- js/constansts/HTMLElements.js
+++ js/constansts/HTMLElements.js
@@ -19,7 +19,7 @@ export const MODALFORM_BUTTONS = MODALFORM.querySelector('.buttons-containers');
 
 
 const X  = `
-  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24"
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
     viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M18 6l-12 12" />
@@ -27,7 +27,7 @@ const X  = `
   </svg>
 `;
 const ARROW_BACK = `
-  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevrons-left" width="24" height="24"
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
     viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M11 7l-5 5l5 5" />
